bcf539788b317b9d54cb7ab807fccd3b228b2339
BT_1N3_06@bktech.local <BT_1N3_06@bktech.local>
2019-02-04 08:39:00 -0500

    Initial commit

---
+++ src/com/company/ChallengeFourSort.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class ChallengeFourSort
+{
+    public static int challengeFour(int[][] arr)
+    {
+        for(int row = 0; row < arr.length; row++)
+        {
+            for(int col = 0; col < arr.length; col++)
+            {
+
+            }
+        }
+    }
+}
